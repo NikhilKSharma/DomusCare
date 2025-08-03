bda56c2bd5f899d28920d8e51bbc351c9b456bd4
NikhilKSharma <144486465+NikhilKSharma@users.noreply.github.com>
2025-08-03 14:25:11 +0530

    tenth

--- flutter_app/lib/core/api/api_client.dart
+++ flutter_app/lib/core/api/api_client.dart
@@ -6,24 +6,24 @@ import 'dart:io' show Platform; // This is safe now because of the kIsWeb guard
 import 'package:http/http.dart' as http;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-String getBaseUrl() {
-  // Check if the app is running on the web
-  if (kIsWeb) {
-    return 'http://localhost:8000/api/v1';
-  }
+// String getBaseUrl() {
+//   // Check if the app is running on the web
+//   if (kIsWeb) {
+//     return 'http://localhost:8000/api/v1';
+//   }
 
-  // If not on web, it's safe to use dart:io's Platform class
-  if (Platform.isAndroid) {
-    return 'http://10.0.2.2:8000/api/v1';
-  } else {
-    // For iOS, desktop, etc.
-    return 'http://localhost:8000/api/v1';
-  }
-}
+//   // If not on web, it's safe to use dart:io's Platform class
+//   if (Platform.isAndroid) {
+//     return 'http://10.0.2.2:8000/api/v1';
+//   } else {
+//     // For iOS, desktop, etc.
+//     return 'http://localhost:8000/api/v1';
+//   }
+// }
 
-// Use the function to get the correct baseUrl
-final String baseUrl = getBaseUrl();
-
+// // Use the function to get the correct baseUrl
+// final String baseUrl = getBaseUrl();
+final String baseUrl = "https://domuscare-api.onrender.com/api/v1";
 // Riverpod provider for the http.Client instance
 final httpClientProvider = Provider<http.Client>((ref) {
   return http.Client();
